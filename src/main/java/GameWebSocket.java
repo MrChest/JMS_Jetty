337f1584500b7715a08315967c166205128c252d
Vasily Chestnov <proggarm@mail.ru>
2017-08-15 21:40:09 +0300

    home

--- src/main/java/GameWebSocket.java
+++ src/main/java/GameWebSocket.java
@@ -1,7 +1,13 @@
-package PACKAGE_NAME;
+import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
 /**
  * Created by chestnov.v on 15.08.2017.
  */
-public class GameWebSocket {
+public class GameWebSocket extends WebSocketServlet{
+
+    @Override
+    public void configure(WebSocketServletFactory webSocketServletFactory) {
+
+    }
 }
